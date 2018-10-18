e8a537aa0edeeab43a8a821ecf46a38b084030cd
OKrylov <krylov92oa@gmail.com>
2018-10-18 23:02:23 +0300

    Lesson 3 - DZ

--- Lesson3/src/Lesson3_DZ_Main.java
+++ Lesson3/src/Lesson3_DZ_Main.java
@@ -17,17 +17,43 @@ public class Lesson3_DZ_Main {
 //            return;
 
         Deque deq = new DequeImpl(6);
-        deq.insertRight(1);
-        deq.insertRight(2);
-        deq.insertRight(3);
-        deq.insertRight(4);// 1 2 3 4
-        deq.insertLeft(5); // 5 1 2 3 4
-        deq.insertLeft(6); // 6 5 1 2 3 4
 
-        deq.removeLeft(); //5 1 2 3 4
-        deq.removeRight(); // 5 1 2 3
+        int value = 1;
+        insertRight(deq, 1);
+        insertRight(deq, 2);
+        insertRight(deq, 3);
+        insertRight(deq, 4);// 1 2 3 4
+        insertLeft(deq, 5); // 5 1 2 3 4
+        insertLeft(deq, 6); // 6 5 1 2 3 4
+
+        removeLeft(deq); //5 1 2 3 4
+        removeRight(deq); // 5 1 2 3
 //        displayLeftToRight(deq); // Queue
         displayRightToLeft(deq); //Stack
+    }
+
+    private static void insertRight(Deque deq, int value) {
+        if ( !deq.isFull() ) {
+            deq.insertRight(value);
+        }
+    }
+
+    private static void insertLeft(Deque deq, int value) {
+        if ( !deq.isFull() ) {
+            deq.insertLeft(value);
+        }
+    }
+
+    private static void removeRight(Deque deq) {
+        if ( !deq.isEmpty() ) {
+            deq.removeRight();
+        }
+    }
+
+    private static void removeLeft(Deque deq) {
+        if ( !deq.isEmpty() ) {
+            deq.removeLeft();
+        }
     }
 
     private static <T> void displayRightToLeft(Deque deq) {
